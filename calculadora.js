6ad7839c842ee88ba6e15321a17a3f9993ee631a
Natalia1428 <naty.ponce5@gmail.com>
2022-05-13 19:50:25 -0400

    se implementa funcionalidad js

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,35 @@
-//Aqui no hay nada
+let buttons = document.querySelectorAll(".mybutton");
+let cuadroResultado = document.getElementById("cresultado");
+
+for (let index = 0; index < buttons.length; index++) {
+  let element = buttons[index];
+  element.addEventListener("click", () => {
+    accion(element.textContent);
+    console.log(element);
+  });
+}
+
+function accion(valor) {
+  if (valor == "AC") {
+    cuadroResultado.value = "";
+  } else if (valor == "=") {
+    cuadroResultado.value = eval(cuadroResultado.value);
+  } else if (
+    !isNaN(valor) ||
+    valor == "+" ||
+    valor == "−" ||
+    valor == "×" ||
+    valor == "÷"
+  ) {
+    if (valor == "−") {
+      valor = "-";
+    }
+    if (valor == "×") {
+      valor = "*";
+    }
+    if (valor == "÷") {
+      valor = "/";
+    }
+    cuadroResultado.value += valor;
+  }
+}
